abd709742e17c5fb76bb2216ca5fec7848df75d2
Iryna Stein <isophiestein@gmail.com>
2021-07-14 12:15:11 -0600

    commit tree 1244d4d8b938ed3cd3128f5ff00a78f3725586a4

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,20 @@
-def find_even_values(src)
+require 'pry'
+
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
 
+
+def find_even_values(src)
+  count = 0
+  while count < src.length
+    inner_count = 0
+    while inner_count < src[count].length
+      if src[count][inner_count].even?
+        puts src [count][inner_count] 
+      end
+      inner_count += 1
+    end
+    count += 1
+  end
 end
